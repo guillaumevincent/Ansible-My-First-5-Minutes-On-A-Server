1c943edf30f2eda9abca51a0fc6115c8f4ee0050
Guillaume Vincent <guillaume@oslab.fr>
2016-09-13 15:31:51 +0200

    remove PermitRootLogin and PasswordAuthentication

--- configure_ssh/test_configure_ssh.sh
+++ configure_ssh/test_configure_ssh.sh
@@ -8,3 +8,15 @@ test_set_ssh_password(){
     run_playbook
     assert "cat /etc/ssh/sshd_config | grep 'Port 2222'" "ssh port is not good"
 }
+
+test_disable_password_authentication(){
+    run_playbook
+    assert_fail "cat /etc/ssh/sshd_config | grep '#PasswordAuthentication'" "PasswordAuthentication should be enable"
+    assert "cat /etc/ssh/sshd_config | grep 'PasswordAuthentication no'" "PasswordAuthentication should be set to no"
+}
+
+test_disable_root_login(){
+    run_playbook
+    assert_fail "cat /etc/ssh/sshd_config | grep '#PermitRootLogin'" "PermitRootLogin should be enable"
+    assert "cat /etc/ssh/sshd_config | grep 'PermitRootLogin no'" "PermitRootLogin should be set to no"
+}
